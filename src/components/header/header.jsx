8a8016c72a3e226a2a3e1dc8a0e28023d3828aa6
Alex Perez <alexgp@Alexs-MacBook-Pro.local>
2023-05-23 10:37:50 -0500

    signup button complete

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -11,12 +11,13 @@ export const Header = () => {
                 <h1 className="text-xl font-medium">EZGO</h1>
             </div>
             <div className="RIGHT flex gap-6">
-                <div className="bg-slate-600 border-2 border-solid border-red-400 rounded-md px-3 py-1">
+                <button className="bg-slate-600 border-2 border-solid border-red-400 rounded-md px-3 py-1">
                     <Link href={'/'}>login</Link>
-                </div>
-                <div className="bg-white hover:bg-gray-100 transition-colors duration-75 ease-in border-2 border-solid border-slate-300 text-slate-950 rounded-md px-3 py-1">
+                </button>
+                <button 
+                   className="card bg-white hover:bg-gray-100 after:blur-md after:animate-pulse border-1 transition-colors duration-75 ease-in text-slate-950 rounded-md px-3 py-1">
                     <Link href={'/'}>signup</Link>
-                </div>
+                </button>
             </div>
         </section>
     )
