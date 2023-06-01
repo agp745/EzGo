a0dcd5b81a6208a260dd595950a7c5d16ff08295
Noah Wright <nwright5252@gmail.com>
2023-05-31 20:53:28 -0500

    map search

--- src/app/view-map/page.js
+++ src/app/view-map/page.js
@@ -1,10 +1,12 @@
 "use client"
 import { DisplayMap } from "@/src/components/map";
 import { Sidebar } from "@/src/components/sidebar";
+import PlacesAutocomplete from "@/src/components/map/autoFinish";
 
 export default function viewMap() {
   return (
     <main className="flex">
+      <PlacesAutocomplete />
       <Sidebar />
       <DisplayMap />
     </main>
--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -4,7 +4,21 @@ import { useMemo, useState, useEffect } from "react"
 import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api"
 import { useSelector } from "react-redux"
 
-function Map({ center }) {
+export function DisplayMap() {
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+        libraries: ["places"]
+    })
+
+    if (!isLoaded) return <div>Loading...</div>
+    return <Map />
+}
+
+function Map() {
+
+    const center = useMemo(() => 
+    ({ lat: 44, lng: -80 }), 
+    [])
 
     return (
     <GoogleMap
