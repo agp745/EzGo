6ed2f983de929130e6aef754d2d9fa9ebf37e05f
Alex Perez <alexgp@Alexs-MBP.attlocal.net>
2023-06-02 17:11:52 -0500

    small change

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useMemo, useState, useEffect } from "react"
+import { useState, useEffect } from "react"
 import { GoogleMap, useLoadScript, Marker, LoadScriptNext, DirectionsRenderer, DirectionsService } from "@react-google-maps/api"
 import { useSelector } from "react-redux"
 import { Sidebar } from "../sidebar"
