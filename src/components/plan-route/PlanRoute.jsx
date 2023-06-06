0f0d575c2c52f447e50fb50ac1c80f356b337200
Noah Wright <nwright5252@gmail.com>
2023-06-06 09:10:04 -0500

    finished

--- src/components/plan-route/PlanRoute.jsx
+++ src/components/plan-route/PlanRoute.jsx
@@ -7,16 +7,15 @@ export function PlanRoute({ routeData }) {
     return (
 <>
   <section className="absolute bottom-2 w-10/12 flex justify-center rounded drop-shadow-xl text-black ">
-    <div className="relative justify-center flex gap-1 bg-white w-[75%] rounded shadow-2xl px-5 py-5">
-      <TransportationChoice />
-
-      {/* Distance and Time */}
-      <div>
-        <p>Time: {routeData.duration && routeData.duration.text}</p>
-        <p>Distance: {routeData.distance && routeData.distance.text}</p>
-      </div>
+  <div className="relative justify-center flex gap-5 bg-white w-[75%] rounded shadow-2xl px-5 py-5">
+    <TransportationChoice />
+    <div className="pl-10">
+      <p className="mb-1"> <span className="text-slate-400"> Time:</span> {routeData.duration && routeData.duration.text}</p>
+      <p> <span className="text-slate-400">Distance:</span> {routeData.distance && routeData.distance.text}</p>
     </div>
-  </section>
+  </div>
+</section>
+
 </>
     )
 }
